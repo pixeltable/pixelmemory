f8e32deb29f5cfd949dbb4919a6a7c510e30a0e5
jacob <jaweiss2305@gmail.com>
2025-07-10 10:24:27 -0600

    redo examples

--- examples/getting_started/02_custom_namespace.py
+++
@@ -1,7 +0,0 @@
-from pixelmemory import Memory
-
-mem = Memory(namespace="my_app_memory", table_name="user_data", if_exists="ignore")
-
-print(f"Successfully created memory table at: {mem.table.memory_id}")
-
-print(f"Table Information: {mem.table.get_metadata()}")
